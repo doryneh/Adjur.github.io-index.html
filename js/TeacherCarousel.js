7dfc7d8b7f095a5bab2c0ff8944e8ec01ba521cb
Dory Nehme <doryy.nehme@gmail.com>
2023-02-07 20:58:56 +0200

    homepage

--- js/TeacherCarousel.js
+++ js/TeacherCarousel.js
@@ -12,57 +12,89 @@ function applyOffsets(items) {
         $(this).css({ '--offset': (index + 1) });
     });
 }
+
 $(document).ready(function () {
-    var inputs = $('input');
-
-    inputs.each(function (index) {
-        $(this).css({
-            'grid-column': (index + 2) + ' / ' + (index + 3),
-            'grid-row': '2 / 3'
+    var inputs = $('section.CarouselPart .carousel-indicator input');
+    var itemElements = $('div.item');
+    
+    inputs.each(function(index) {
+      $(this).css({
+        'grid-column': (index + 2) + ' / ' + (index + 3),
+        'grid-row': '2 / 3'
+      });
+    
+      $(this).click(function() {
+        var position = index + 1;
+        $('main#carousel').css({'--position': position});
+        itemElements.each(function(itemIndex) {
+          if (itemIndex == index) {
+            $(this).css({'--offset': position});
+          }
         });
-
-        $(this).click(function () {
-            var position = index + 1;
-            $('main#carousel').css({ '--position': position });
-        });
+      });
     });
-
-
+    
+    itemElements.click(function() {
+      var position = $(this).index() + 1;
+      $('main#carousel').css({'--position': position});
+    });
+    
     var items = $('div.item');
     applyOffsets(items);
-
+    
     setCarouselProperties(3);
-
+    
     $('section.CarouselPart .carousel-indicator input:checked').parent('section.CarouselPart .carousel-indicator').css('opacity', '100%');
-
+    
     $('  section.CarouselPart .carousel-indicator input').on('change', function () {
-        $(' section.CarouselPart .carousel-indicator').css('opacity', '50%');
-
+        $('section.CarouselPart .carousel-indicator').css('opacity', '50%');
         $('section.CarouselPart .carousel-indicator input:checked').parent('section.CarouselPart .carousel-indicator').css('opacity', '100%');
     });
-
-
-
+    
     /*Prev and next Buttons */
+    var $carousel = $('main#carousel');
+    var items = parseInt($carousel.css('--items'));
+    var middle = parseInt($carousel.css('--middle'));
+    var position = parseInt($carousel.css('--position'));
+    
     $('#NextBtn').click(function() {
-        $('section.CarouselPart .carousel-indicator :radio[name="position"]').each(function(index) {
-            if ($(this).is(':checked')) {
-            $(this).removeAttr('checked');
-            $(':radio[name="position"]').eq(index + 1).attr('checked', 'checked');
-            return false;
-          }
-        });
+        position = (position + 1) % items;
+        if (position < 0) {
+          position += items;
+        }
+        $carousel.css('--position', position + 1);
+    
+        var currentPosition = parseInt($carousel.css('--position'));
+        var totalItems = parseInt($carousel.css('--items'));
+        if (currentPosition > totalItems) {
+          $carousel.css({'--position': totalItems });
+        }
+      });
+    
+      $('#PrevBtn').click(function() {
+        position = (position - 1 + items) % items;
+        $carousel.css('--position', position + 1);
+        var currentPosition = parseInt($carousel.css('--position'));
+    
+        if (currentPosition < 1) {
+          $carousel.css({'--position': 1});
+        }
       });
 
-      $('#PrevBtn').click(function() {
-        $('section.CarouselPart .carousel-indicator :radio[name="position"]').each(function(index) {
-            if ($(this).is(':checked')) {
-            $(this).removeAttr('checked');
-            $(':radio[name="position"]').eq(index - 1).attr('checked', 'checked');
-            return false;
-          }
-        });
-      });
+      // setInterval function to increase position every 7 seconds
+       setInterval(function() {
+        position = (position + 1) % items;
+        if (position < 0) {
+          position += items;
+        }
+        $carousel.css('--position', position + 1);
+    
+        var currentPosition = parseInt($carousel.css('--position'));
+        var totalItems = parseInt($carousel.css('--items'));
+        if (currentPosition > totalItems) {
+          $carousel.css({'--position': totalItems });
+        }
+      }, 7000);
 
 
 });
--- js/js.js
+++ js/js.js
@@ -4,22 +4,23 @@ $(document).ready(function () {
   });
   $('section.Bundels .Prev').click(function () {
     $('#carouselExample').carousel('prev');
+  });  
+  $(".dropdown-toggle").click(function() {
+    $(".dropdown-menu").toggle();
   });
-
-
-  $(document).ready(function () {
-    $('.d-flex .purpleBtn').click(function () {
-      var button = $(this);
-      if (button.hasClass('AddedToCart')) {
-        button.removeClass('AddedToCart');
-        button.html('<img src="images/bag-2.svg" alt="">');
-      } else {
-        button.addClass('AddedToCart');
-        button.html('<img src="images/AddedToCart.png" alt="">');
-      }
-    });
-
-  });
-
-
 });
+
+$(document).ready(function () {
+
+  $('.d-flex .purpleBtn').click(function () {
+    var button = $(this);
+    if (button.hasClass('AddedToCart')) {
+      button.removeClass('AddedToCart');
+      button.html('<img src="images/bag-2.svg" alt="">');
+    } else {
+      button.addClass('AddedToCart');
+      button.html('<img src="images/AddedToCart.png" alt="">');
+    }
+  });
+});
+
